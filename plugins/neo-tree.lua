a1f00599a673eb9f778369666921900ae7c02aa7
NemuriNeko <98445083+shingoo0812@users.noreply.github.com>
2024-02-08 21:38:49 +0900

    Initial commit

---
+++ plugins/neo-tree.lua
@@ -0,0 +1,19 @@
+return {
+  "neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        --visible = true,
+        -- hide_dotfiles = false,
+        -- hide_gitignored = true,
+        -- hide_by_name = {
+        --   ".github",
+        --   ".gitignore",
+        --   "package-lock.json",
+        -- },
+        -- never_show = { ".git" },
+        visible = true,
+      },
+    },
+  },
+}
---
+++ plugins/user.lua
@@ -0,0 +1,38 @@
+return {
+  -- You can also add new plugins here as well:
+  -- Add plugins, the lazy syntax
+  -- "andweeb/presence.nvim",
+  -- {
+  --   "ray-x/lsp_signature.nvim",
+  --   event = "BufRead",
+  --   config = function()
+  --     require("lsp_signature").setup()
+  --   end,
+  -- },
+  {
+    "github/copilot.vim",
+    lazy = false,
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Une for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+  },
+  {
+    "ggandor/leap.nvim",
+    event = "VeryLazy",
+  },
+  {
+    "mg979/vim-visual-multi",
+    event = "VeryLazy",
+  },
+   {
+    "jesseduffield/lazygit",
+    event = "VeryLazy",
+  },
+} 
