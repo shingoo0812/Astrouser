4d93f36281b564f17ef131cfeb7682c79486f023
shingoo0812 <shingoo0812@gmail.com>
2024-03-16 21:03:11 +0900

    test

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,3 +1,4 @@
+-- TODO: fix show todo list
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
@@ -11,28 +12,22 @@ return {
   -- },
   {
     "github/copilot.vim",
+    "ggandor/leap.nvim",
+    "mg979/vim-visual-multi",
+    "jesseduffield/lazygit",
     lazy = false,
   },
   {
     "kylechui/nvim-surround",
     version = "*", -- Une for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
-    config = function()
-        require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use defaults
-        })
-    end
   },
-  {
-    "ggandor/leap.nvim",
-    event = "VeryLazy",
-  },
-  {
-    "mg979/vim-visual-multi",
-    event = "VeryLazy",
-  },
-   {
-    "jesseduffield/lazygit",
-    event = "VeryLazy",
-  },
+--  {
+--    "folke/todo-comments.nvim",
+--    lazy = false,
+--    dependencies = { "nvim-lua/plenary.nvim" },
+--    config = function()
+--      require("todo-comments").setup()
+--    end,
+--  },
 }
