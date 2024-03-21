85f59d4003e622a4dd083270176f003eb3fda2b5
shingoo0812 <shingoo0812@gmail.com>
2024-03-22 08:47:34 +0900

    modify surround

--- plugins/user.lua
+++ plugins/user.lua
@@ -7,8 +7,17 @@ return {
     "ggandor/leap.nvim",
     "mg979/vim-visual-multi",
     "jesseduffield/lazygit",
-    "kylechui/nvim-surround",
     event = "VeryLazy",
+  },
+  {
+      "kylechui/nvim-surround",
+      version = "*", -- Use for stability; omit to use `main` branch for the latest features
+      event = "VeryLazy",
+      config = function()
+          require("nvim-surround").setup({
+              -- Configuration here, or leave empty to use defaults
+          })
+      end
   },
   { "catppuccin/nvim",
     name = "catppuccin",
